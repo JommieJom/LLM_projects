f0e27bf9ec952a06c6a9047c29af32067cd9d6a5
jommiejom <jombhanut@gmail.com>
2025-01-31 11:09:03 +0700

    add test.py

---
+++ test.py
@@ -0,0 +1,22 @@
+import requests
+
+url = "http://localhost:8000/send_message"
+
+data = {
+    "message": "How many person in db who are male, please handle case sensitive as well",
+    # "db_path": "databases/sleep.db"
+}
+
+response = requests.post(url, json=data)
+print(response.json())
+
+# url = "http://localhost:8000/execute_query"
+# data = {
+#     "db_path": "databases/example.db",
+#     "query": "SELECT * FROM customers WHERE city = 'New York'",
+#     "limit": 5,
+#     "offset": 0
+# }
+
+# response = requests.post(url, json=data)
+# print(response.json())
